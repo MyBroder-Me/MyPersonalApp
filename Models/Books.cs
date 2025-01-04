480fa9080e434f5f152188e11e0683ec79ba1566
Daniel Márquez <daniel.marquez@ciclosmontecastelo.com>
2025-01-04 22:25:52 +0100

    feat: database connect, books

---
+++ Models/Books.cs
@@ -0,0 +1,24 @@
+﻿
+using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+
+namespace MyApp.Models;
+
+[Table("books")]
+public class Book : BaseModel
+{
+    [PrimaryKey("id", false)]
+    public string Id { get; set; }
+
+    [Column("title")]
+    public string Title { get; set; }
+
+    [Column("author")]
+    public string Author { get; set; }
+
+    [Column("created_at", ignoreOnInsert: true)]
+    public DateTime CreatedAt { get; set; }
+
+    [Column("is_finished")]
+    public bool IsFinished { get; set; }
+}
---
+++ Services/IDataService.cs
@@ -0,0 +1,11 @@
+﻿using MyApp.Models;
+
+namespace MyApp.Services;
+
+public interface IDataService
+{
+    Task<IEnumerable<Book>> GetBooks();
+    Task CreateBook(Book book);
+    Task DeleteBook(string id);
+    Task UpdateBook(Book book);
+}
