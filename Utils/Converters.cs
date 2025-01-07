3275b61a07d42d6a1d495620dd7adad80e9ad8b6
Daniel Márquez <daniel.marquez@ciclosmontecastelo.com>
2025-01-07 19:42:17 +0100

    Refactoring of Converters into a Single File A new UrlConverter has been added to the resource dictionary in App.xaml. The file StringNullOrEmptyToVisibilityConverter.cs containing the implementation of the StringNullOrEmptyToVisibilityConverter has been removed. The file UrlConverter.cs containing the implementation of the UrlConverter has been removed. The declaration of the UrlConverter in the resources of UpdateBookPage.xaml has been removed. A new file Converters.cs has been created, which now contains the implementations of the UrlConverter and StringNullOrEmptyToVisibilityConverter converters.

--- Utils/Converters.cs
+++ Utils/Converters.cs
@@ -18,4 +18,20 @@ namespace MyApp.Utils
             throw new NotImplementedException();
         }
     }
+    public class StringNullOrEmptyToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string str)
+            {
+                return !string.IsNullOrEmpty(str);
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
