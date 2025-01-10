1d0d0db4417ecff374fe3bf0826e09fa796d5b46
Mykyta Ryasny <MykytaRyasny@gmail.com>
2025-01-11 00:03:58 +0100

    Feat: added linter, fixed some issues

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,8 +5,9 @@ import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
-
+import React from 'react';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import SpaceMonoFont from '@/assets/fonts/SpaceMono-Regular.ttf';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -14,7 +15,7 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: SpaceMonoFont,
   });
 
   useEffect(() => {
