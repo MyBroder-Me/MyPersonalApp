8d958c68ddedb82242b262a2522501293197c9bf
Daniel Márquez <daniel.marquez@ciclosmontecastelo.com>
2025-01-10 23:18:50 +0100

    Refactor HelloWave component to use useFocusEffect for animation

--- components/HelloWave.tsx
+++ components/HelloWave.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet } from 'react-native';
 import Animated, {
   useSharedValue,
@@ -7,18 +7,21 @@ import Animated, {
   withRepeat,
   withSequence,
 } from 'react-native-reanimated';
+import { useFocusEffect } from '@react-navigation/native';
 
 import { ThemedText } from '@/components/ThemedText';
 
 export function HelloWave({ emoji }: { emoji: string }) {
   const rotationAnimation = useSharedValue(0);
 
-  useEffect(() => {
-    rotationAnimation.value = withRepeat(
-      withSequence(withTiming(25, { duration: 150 }), withTiming(0, { duration: 150 })),
-      4 // Run the animation 4 times
-    );
-  }, []);
+  useFocusEffect(
+    React.useCallback(() => {
+      rotationAnimation.value = withRepeat(
+        withSequence(withTiming(25, { duration: 150 }), withTiming(0, { duration: 150 })),
+        4 // Run the animation 4 times
+      );
+    }, [])
+  );
 
   const animatedStyle = useAnimatedStyle(() => ({
     transform: [{ rotate: `${rotationAnimation.value}deg` }],
