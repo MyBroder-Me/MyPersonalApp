252cb236066bf0f0854e0b744c7de248ee864c55
Daniel Márquez <daniel.marquez@ciclosmontecastelo.com>
2025-01-10 23:11:09 +0100

    Add Books screen and integrate environment variables

--- components/ui/IconSymbol.tsx
+++ components/ui/IconSymbol.tsx
@@ -13,6 +13,8 @@ const MAPPING = {
   'paperplane.fill': 'send',
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
+  'book': 'book',
+  'menu-book': 'menu-book',
 } as Partial<
   Record<
     import('expo-symbols').SymbolViewProps['name'],
