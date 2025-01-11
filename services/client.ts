a026bb807037e19a9c55f5de8994a3f06922da5b
Mykyta Ryasny <MykytaRyasny@gmail.com>
2025-01-11 11:31:57 +0100

    Chore: Build and Readme update

--- services/client.ts
+++ services/client.ts
@@ -1,7 +1,7 @@
-import { createClient } from '@supabase/supabase-js'
-import { Database } from '@/types/database.types'
-import { SUPABASE_URL, SUPABASE_ADMIN } from '@env'
+import { createClient } from '@supabase/supabase-js';
+import { Database } from '@/types/database.types';
+import { SUPABASE_URL, SUPABASE_ADMIN } from '@env';
 
 export const supabase = createClient<Database>(
-    SUPABASE_URL, SUPABASE_ADMIN
-)
+  SUPABASE_URL, SUPABASE_ADMIN
+);
